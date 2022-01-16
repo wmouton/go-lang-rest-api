569e1586f7858cbc7e66b248b9cbc2e7721da555
l33th <l33th@protonmail.ch>
2022-01-16 07:58:26 +0200

    gorilla mux fix

--- main.go
+++ main.go
@@ -39,11 +39,11 @@ func main() {
 	router := mux.NewRouter()
 
 	// Route Handlers / Endpoints
-	router.HandleFunc("/api/books", getBooks).Method("GET")
-	router.HandleFunc("/api/books/{id}", getBook).Method("GET")
-	router.HandleFunc("/api/books", createBook).Method("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Method("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Method("DELETE")
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
