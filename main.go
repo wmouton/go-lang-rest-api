2f571e10a5d09197efa83d109ca88230c176cebb
l33th <l33th@protonmail.ch>
2022-01-16 07:34:15 +0200

    added imports

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
