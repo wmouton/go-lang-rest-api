466a94992e66f802f3abb1991ce6beb2eff720ba
l33th <l33th@protonmail.ch>
2022-01-16 08:21:18 +0200

    getting a single book route

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/l33th/go-lang-rest-api
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -25,22 +25,42 @@ type Author struct {
 var books []Book
 
 // Get All Books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func getBooks(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(books); err != nil {
 		log.Fatalln(err, "Error")
 	}
 }
-// Get A Book
-func getBook(w http.ResponseWriter, r *http.Request) {}
+// Get A Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			if err := json.NewEncoder(w).Encode(item); err != nil {
+				log.Fatalln(err, "Error")
+			}
+			return
+		}
+	}
+	if err := json.NewEncoder(w).Encode(&Book{}); err != nil {
+		log.Fatalln(err, "Error")
+	}
+}
+
 // Create A Book
-func createBook(w http.ResponseWriter, r *http.Request) {}
+func createBook(_ http.ResponseWriter, _ *http.Request) {}
+
 // Update A Book
-func updateBook(w http.ResponseWriter, r *http.Request) {}
+func updateBook(_ http.ResponseWriter, _ *http.Request) {}
+
 // Delete A Book
-func deleteBook(w http.ResponseWriter, r *http.Request) {}
+func deleteBook(_ http.ResponseWriter, _ *http.Request) {}
 
 func main() {
+	// Server Message On Startup
+	log.Println("sever started on port 8080...")
 	// Init Router
 	router := mux.NewRouter()
 
